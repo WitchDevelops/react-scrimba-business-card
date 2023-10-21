ffd73c967915de0a20ae844cdb91c57579015e89
WitchDevelops <d.n.wojewska@gmail.com>
2023-10-21 10:30:14 +0200

    final touches

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Footer from "./components/Footer";
 import "./App.css";
 const App = () => {
   return (
-    <div className="app__card">
+    <div className="app__wrapper">
+      <div className="app__card">
       <Header />
       <Main />
       <Footer />
+    </div>
     </div>
   );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,32 @@
+import linkedin from "../assets/linkedin.svg";
+import portfolio from "../assets/website.svg";
+import blog from "../assets/blog.svg";
+import github from "../assets/github.svg";
 import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2023 Dominika Wojewska</p>
+      <div className="footer__icons">
+        <a
+          className="footer__link"
+          href="https://www.linkedin.com/in/dominika-wojewska/"
+        >
+          <img className="footer__icon" src={linkedin}></img>
+        </a>
+        <a className="footer__link" href="https://mokosh.dev/">
+          <img className="footer__icon" src={portfolio}></img>
+        </a>
+        <a className="footer__link" href="https://blog.mokosh.dev/">
+          <img className="footer__icon" src={blog}></img>
+        </a>
+        <a className="footer__link" href="https://github.com/WitchDevelops">
+          <img className="footer__icon" src={github}></img>
+        </a>
+      </div>
+      <hr className="footer__line"></hr>
+      <p className="footer__text">© 2023 Dominika Wojewska</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
